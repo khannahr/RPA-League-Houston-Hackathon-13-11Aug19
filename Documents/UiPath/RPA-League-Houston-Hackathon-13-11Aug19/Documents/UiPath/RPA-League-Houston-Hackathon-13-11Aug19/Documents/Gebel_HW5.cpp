36962d1f4f2a022be32c3e92e8b3877d37f20517
Hannah Ritchie <kathryn.ritchie@mavs.uta.edu>
2019-08-11 11:12:24 -0500

    HR

---
+++ Documents/UiPath/RPA-League-Houston-Hackathon-13-11Aug19/Documents/UiPath/RPA-League-Houston-Hackathon-13-11Aug19/Documents/Gebel_HW5.cpp
@@ -0,0 +1,27 @@
+//Program Name: Name in Stars
+//Purpose: To familiarize and help display semi-formatted text
+//Author: Kristina Gebel
+//Date Last Modified: 1/27/2019
+
+//Shape Calculator is a program that can perform different mathematical computations for various geometrical shapes.
+
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+# include <iostream>
+using namespace std;
+int main () {
+	
+double radius = 10;
+//area of the circle from the formula: M_PI * 10 * 10
+double area= M_PI * 10 * 10;
+//Welcome to the Shape Calculator
+cout << "Welcome to the Shape Calculator"<< endl;
+
+//The area of the circle is: 314.159
+cout << "The area of the circle is: " << area << endl;
+
+
+
+return 0;
+}
---
+++ Documents/UiPath/RPA-League/Documents/UiPath/RPA-League/Documents/UiPath/RPA-League/Documents/UiPath/RPA-League/Documents/Gebel_HW7.cpp
@@ -0,0 +1,21 @@
+//Program Name: Minimum and Maximum
+//Purpose: This is a simple program to determine and return the lowest and highest values of two numbers. 
+//Author: Kristina Gebel
+//Date Last Modified: 1/27/2019
+
+//Minimum and Maximum Program 
+
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+# include <iostream>
+using namespace std;
+int main () {
+double input1 = 65;//double input1 and initialize it with 65
+double input2 = 23;//double input2 and initialize it with 23
+double minimumValue = fmin(input1, input2); //double minimumValue (lower inputted value)
+double maximumValue=fmax(input1,input2); //double maximumValue (higher inputted value
+cout << "Welcome to Minimum and Maximum!" <<  endl;
+cout << "The number " << input2 << " is lower than "<< input1 << endl;
+return 0;
+}
